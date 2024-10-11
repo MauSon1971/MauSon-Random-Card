cd4d80fd17da7c507b0204ceb28352fcf77152b7
MauSon1971 <110358138+MauSon1971@users.noreply.github.com>
2024-10-11 23:34:53 +0000

    1er.Cmt - Layout + logica de carta aleatoria

--- src/app.js
+++ src/app.js
@@ -7,5 +7,45 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const numeros = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  const palos = ["♦", "♥", "♠", "♣"];
+  const baraja = [];
+
+  numeros.forEach(numero => {
+    palos.forEach(palo => {
+      baraja.push(`${numero}${palo}`);
+    });
+  });
+  console.log("Baraja Inicial", baraja);
+  console.log("Cartas de la Baraja", baraja.length);
+
+  function generaCarta() {
+    let cartaAleatoria = baraja[Math.floor(Math.random() * baraja.length)];
+    let numeroAleatorio = cartaAleatoria.slice(0, -1);
+    let paloAleatorio = cartaAleatoria.slice(-1);
+    console.log("Carta seleccionada:", cartaAleatoria);
+    console.log("Número seleccionado:", numeroAleatorio);
+    console.log("Palo seleccionado:", paloAleatorio);
+  }
+
+  document.getElementById("boton1").addEventListener("click", generaCarta);
+
+  /*
+  1) Array de 10 números + array de 3 letras + array de 4 figuras
+
+  */
 };
