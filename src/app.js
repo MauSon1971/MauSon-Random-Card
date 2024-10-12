3323dcf4fb91147d4f816f1f99bea323de2de9da
MauSon1971 <110358138+MauSon1971@users.noreply.github.com>
2024-10-12 00:33:51 +0000

    2do.Cmnt - Logica color y aleatoriedad terminada

--- src/app.js
+++ src/app.js
@@ -34,15 +34,37 @@ window.onload = function() {
   console.log("Cartas de la Baraja", baraja.length);
 
   function generaCarta() {
+    //Genero carta aleatoria y separa el numero del palo en un objeto
     let cartaAleatoria = baraja[Math.floor(Math.random() * baraja.length)];
     let numeroAleatorio = cartaAleatoria.slice(0, -1);
     let paloAleatorio = cartaAleatoria.slice(-1);
     console.log("Carta seleccionada:", cartaAleatoria);
     console.log("Número seleccionado:", numeroAleatorio);
     console.log("Palo seleccionado:", paloAleatorio);
+    return { numero: numeroAleatorio, palo: paloAleatorio };
   }
 
-  document.getElementById("boton1").addEventListener("click", generaCarta);
+  // document.getElementById("boton1").addEventListener("click", generaCarta);
+
+  function mostrarCarta(carta) {
+    //si carta.palo es = a corazon o diamante => color rojo y si no negro.
+    let colorCarta =
+      carta.palo === "♥" || carta.palo === "♦" ? "text-danger" : "text-dark";
+    //Pinto la carta con el palo y numero.
+    document.getElementById("palo1").textContent = carta.palo;
+    document.getElementById("numero").textContent = carta.numero;
+    document.getElementById("palo2").textContent = carta.palo;
+    //Pinto la carta con el color del palo
+    document.getElementById("palo1").className = `display-6 ${colorCarta}`;
+    document.getElementById(
+      "numero"
+    ).className = `display-1 fw-bold ${colorCarta}`;
+    document.getElementById("palo2").className = `display-6 ${colorCarta}`;
+  }
+  document.getElementById("boton1").addEventListener("click", function() {
+    const carta = generaCarta(); // Llamamos a generaCarta para obtener una carta aleatoria
+    mostrarCarta(carta); // Usamos esa carta en otra función
+  });
 
   /*
   1) Array de 10 números + array de 3 letras + array de 4 figuras
